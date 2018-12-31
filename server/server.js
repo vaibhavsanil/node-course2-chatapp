d82191ec065a2a7f43693a222b41d0c958d5b9fc
Vaibhav Sanil <vaibhav.sanil007@gmail.com>
2018-12-31 14:36:24 +0530

    Added event listeners to serverjs file

--- server/server.js
+++ server/server.js
@@ -14,19 +14,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New User Connected!!!');
 
-	socket.emit('newMessage', {
+/*	socket.emit('newMessage', {
 		from:'mike-9787454658',
 		text:'Hey what is going on',
 		createAt:123
-	});
+	});*/
 
 	socket.on('createMessage',(message) => {
 		console.log('createMessage',message);
+
+		io.emit('newMessage', {
+			from: message.from,
+			text:message.text,
+			createAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect',()=>{
 	console.log('User was disconnected!!!');
-});
+       });
 });
 
 
